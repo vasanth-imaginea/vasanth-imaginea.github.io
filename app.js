17244b5db13f983c7d4e2f76790d6d662293a043
Vasanth <vasanthraja1011@gmail.com>
2015-07-17 13:38:25 +0530

    Removed JSDOC

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var app = angular.module('search', []);
 
 app.controller('MainCtrl', function($scope,$http) {
-  $http.get('configure.json').success(function(response){ /* Fetching the JSON data */
+  $http.get('configure.json').success(function(response){ 
     $scope.configs=response;
-    $scope.selectedOperation=response.operations[0] /* Selecting the first element by default */
+    $scope.selectedOperation=response.operations[0] 
   });
   $scope.submit=function(){
     $scope.result="";
     var selectedOperation=angular.copy($scope.selectedOperation);
     selectedOperation.options=selectedOperation.options.concat(
-     $scope.configs.common_operations.options) /* Merging the common options with operation specific options */
+     $scope.configs.common_operations.options) 
     $scope.result+=selectedOperation.operation_name;
     for(var  i=0;i<selectedOperation.options.length;i++){
       if(selectedOperation.options[i].checked){
         $scope.result+=" "+selectedOperation.options[i].value
       }
   }
-     $scope.result+=" "+($scope.searchText || "")/* Concatenating the values of checked fields and displaying */
+     $scope.result+=" "+($scope.searchText || "")
   }
 });
