727222124b4d812d1b28a6bf44f8acc19e657bac
Vasanth <vasanthraja1011@gmail.com>
2015-07-17 13:45:24 +0530

    Modified JS

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var app = angular.module('search', []);
+var app = angular.module('plunker', []);
 
 app.controller('MainCtrl', function($scope,$http) {
-  $http.get('configure.json').success(function(response){ 
+  $http.get('configure.json').success(function(response){
     $scope.configs=response;
-    $scope.selectedOperation=response.operations[0] 
+    $scope.selectedOperation=response.operations[0]
   });
   $scope.submit=function(){
     $scope.result="";
     var selectedOperation=angular.copy($scope.selectedOperation);
     selectedOperation.options=selectedOperation.options.concat(
-     $scope.configs.common_operations.options) 
+     $scope.configs.common_operations.options)
     $scope.result+=selectedOperation.operation_name;
     for(var  i=0;i<selectedOperation.options.length;i++){
       if(selectedOperation.options[i].checked){
