33b8d4721d0b81506aa08d87fba9bb9287a482dd
Vasanth <vasanthraja1011@gmail.com>
2015-07-17 13:17:03 +0530

    Removed setOperations function to attach the object directly

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 var app = angular.module('search', []);
 
 app.controller('MainCtrl', function($scope,$http) {
-  $scope.name = 'World';
-  $scope.result="";
   $http.get('configure.json').success(function(response){
     $scope.configs=response;
-    $scope.selOperation=response.operations[0].operation_name
-    $scope.setOperation(response.operations[0])
+    $scope.selectedOperation=response.operations[0]
   });
-  $scope.setOperation=function(operation){
-    $scope.selectedOperation=operation;
-    //$scope.selectedOperation.options=$scope.selectedOperation.options.concat(
-    //  $scope.configs.common_operations.options)
-  }
   $scope.submit=function(){
     $scope.result="";
     var selectedOperation=angular.copy($scope.selectedOperation);
@@ -23,9 +15,7 @@ app.controller('MainCtrl', function($scope,$http) {
       if(selectedOperation.options[i].checked){
         $scope.result+=" "+selectedOperation.options[i].value
       }
-   
   }
-     $scope.result+=" "+$scope.searchText
+     $scope.result+=" "+($scope.searchText || "")
   }
 });
-//
